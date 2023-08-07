4391bcd223d3031ec80376a3fb8ebe87ecde3bea
Cunn1 <141624189+Cunn1@users.noreply.github.com>
2023-08-07 23:45:57 +0300

    Add files via upload

--- if.js
+++ if.js
@@ -2,9 +2,9 @@ let kullaniciAdi = prompt("Kullanici adinizi girin:");
 let sifre = prompt("Şifrenizi girin:");
 
 if (kullaniciAdi === "admin" && sifre === "12345") {
-  console.log("Giriş başarili. Hoş geldiniz, Admin!");
+  console.log("Giriş başarili Hoş geldiniz Admin");
 } else if (kullaniciAdi === "user" && sifre === "6789") {
-  console.log("Giriş başarili Hoş geldiniz Kullanici!");
+  console.log("Giriş başarili Hoş geldiniz Kullanici");
 } else {
-  console.log("Giriş başarisiz Geçersiz kullanici adi veya şifre.");
+  console.log("Giriş başarisiz Geçersiz kullanici adi veya şifre");
 }
