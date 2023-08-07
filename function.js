387db70d38d111af058cc3f66db2d60bb24509a0
Cunn1 <141624189+Cunn1@users.noreply.github.com>
2023-08-07 22:45:29 +0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,20 @@
+function sayiuret(){
+
+ return Math.ceil(Math.random() * 49)
+
+
+
+}
+sayiuret()
+
+var sayi1=sayiuret()
+var sayi2=sayiuret()
+var sayi3=sayiuret()
+var sayi4=sayiuret()
+var sayi5=sayiuret()
+var sayi6=sayiuret()
+
+
+
+
+console.log(sayi1,sayi2,sayi3,sayi4,sayi5,sayi6);
