eea7c000fa5907aaff62eda9cc0125f5156a722f
Cunn1 <141624189+Cunn1@users.noreply.github.com>
2023-08-24 15:47:14 +0300

    Add files via upload

---
+++ CurrencyCalculator.js
@@ -0,0 +1,45 @@
+const amount = document.querySelector("#amount");
+const FirstOption = document.querySelector("#firstCurrencyOption");
+const SecondtOption = document.querySelector("#secondCurrencyOption");
+const resultInput = document.querySelector(`#result`);
+
+const Currency = {
+    USD: 27,
+    EUR: 32,
+    JPY: 130,
+    STR: 35,
+    TL: 1,
+};
+
+function exchange() {
+    if (amount.value < 0) {
+        alert("lütfen geçerli bir değer giriniz")
+        return;
+    }
+    console.log(Currency[firstCurrencyOption.value]);
+    const result = amount.value * Currency[firstCurrencyOption.value]
+    console.log(result);
+    resultInput.value = result;
+}
+function runEventListeners() {
+    amount.addEventListener("input", exchange)
+    firstCurrencyOption.addEventListener("change", exchange);
+    secondCurrencyOption.addEventListener("change", exchange);
+}
+
+runEventListeners();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
