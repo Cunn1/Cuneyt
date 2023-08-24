5327ac6d87656adab79a4339676059b51cd53a1d
Cunn1 <141624189+Cunn1@users.noreply.github.com>
2023-08-24 16:29:50 +0300

    CurrencyCalculator.js

--- CurrencyCalculator.js
+++ CurrencyCalculator.js
@@ -1,4 +1,4 @@
-const amount = document.querySelector("#amount");
+const amountInput = document.querySelector("#amount");
 const FirstOption = document.querySelector("#firstCurrencyOption");
 const SecondtOption = document.querySelector("#secondCurrencyOption");
 const resultInput = document.querySelector(`#result`);
@@ -9,39 +9,29 @@ const Currency = {
     JPY: 130,
     STR: 35,
     TL: 1,
-};
+}
 
 function exchange() {
-    if (amount.value < 0) {
+    if (amountInput.value < 0) {
+        amountInput.value = "";
         alert("lütfen geçerli bir değer giriniz")
         return;
     }
     console.log(Currency[firstCurrencyOption.value]);
-    const result = amount.value * Currency[firstCurrencyOption.value]
+    const result = amountInput.value * Currency[firstCurrencyOption.value]
     console.log(result);
     resultInput.value = result;
-    button.addEventListener("click",exchange)
+    button.addEventListener("click", exchange)
+    amountInput.addEventListener('input', function () {
+        amountInput.value = amountInput.value.replace(/-/g, ''); 
+    });
+
 }
 function runEventListeners() {
-    button.addEventListener("click",exchange)
+    button.addEventListener("click", exchange)
     // amount.addEventListener("input", exchange)
     firstCurrencyOption.addEventListener("change", exchange);
     secondCurrencyOption.addEventListener("change", exchange);
 }
 
 runEventListeners();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
